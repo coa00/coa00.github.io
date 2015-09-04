b83938665762bf9502048e78cd927bdbed61f948
Kohei Aoki <coa.andu@gmail.com>
2015-09-05 03:05:27 +0900

    add p5.js

--- js/sketch.js
+++ js/sketch.js
@@ -1,6 +1,6 @@
 var pos = 100;
 function setup(){
-    var myCanvas = createCanvas(windowWidth, 200);
+    var myCanvas = createCanvas(windowWidth, 300);
     myCanvas.parent('myContainer');
     background(256);
     line(0, 0, windowHeight, windowHeight);
@@ -15,11 +15,9 @@ function windowResized() {
 function mouseWheel(event) {
     //event.delta can be +1 or -1 depending
     //on the wheel/scroll direction
-    print(event.delta);
     //move the square one pixel up or down
     pos += event.delta;
 
-    print(pos);
     //uncomment to block page scrolling
     //return false;
 }
@@ -31,7 +29,7 @@ function draw() {
     for (var x=0; x < windowWidth; x++) {
         var noiseVal = noise((pos+x)*noiseScale,
             pos*noiseScale);
-        stroke(noiseVal*255*1.5);
+        stroke(noiseVal*255*1.5,244,244);
         line(x, pos+noiseVal*80, x, windowHeight);
     }
 }
